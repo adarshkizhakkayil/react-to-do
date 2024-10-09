1d846a278c94635fee16184e897e98f0639aa519
adarshkizhakkayil <adarshk09052001@gmail.com>
2024-10-09 05:43:33 +0530

    completed

---
+++ src/components/TodoForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+export const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (value.trim() === '') {
+            setError('Please enter a task');
+            return;
+        }
+        addTodo(value);
+        setValue(''); 
+        setError(''); 
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="TodoForm">
+            <input 
+                type="text" 
+                value={value} 
+                onChange={(e) => setValue(e.target.value)} 
+                className="todo-input" 
+                placeholder='What is the task today?' 
+            />
+            <button type="submit" className='todo-btn'>Add Task</button>
+            {error && <p className="error-message">{error}</p>} {/* Display error message */}
+        </form>
+    );
+};
